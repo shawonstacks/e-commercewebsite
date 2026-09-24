61db064ae7ca74b88ecdc9053cd5b088da4e8ca4
shawonstacks <immehedihasans2200@gmail.com>
2026-09-24 16:32:12 +0600

    Update Auth

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  phone: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['customer', 'admin'], default: 'customer' }
-});
+  name: { 
+    type: String 
+  },
+  phone: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  role: { 
+    type: String, 
+    enum: ['customer', 'admin'], 
+    default: 'customer' 
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
